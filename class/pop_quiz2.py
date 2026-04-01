8bd0f9fae3e1512dd1cf73d851b6245e8653db99
Kush Patel <kushpatel1836@gmail.com>
2026-04-01 13:43:00 -0400

    pop quiz 2

---
+++ class/pop_quiz2.py
@@ -0,0 +1,6 @@
+# Question: What are the time complexities of the following operations for a graph: (adjaceny list representation)
+# Briefly explain the reason. 
+# 1: Add a node:
+# 2: remove a node:
+# 3: Check if two nodes are adjacent or not.
+#  save a word doc on desktop, and submit it on iCollege. 
